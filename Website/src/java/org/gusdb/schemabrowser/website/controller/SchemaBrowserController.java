a24e11cdaf179324a52e5793177628eb45ebf963
Michael Saffitz <m@saffitz.com>
2005-08-11 17:57:01 +0000

    added column doc population back in after it stopped working
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/SchemaBrowser/trunk@3287 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Website/src/java/org/gusdb/schemabrowser/website/controller/SchemaBrowserController.java
+++ Website/src/java/org/gusdb/schemabrowser/website/controller/SchemaBrowserController.java
@@ -9,6 +9,7 @@ import java.util.Iterator;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.gusdb.dbadmin.model.GusTable;
+import org.gusdb.dbadmin.model.GusColumn;
 import org.gusdb.schemabrowser.website.DatabaseFactory;
 import org.gusdb.schemabrowser.website.dao.DocumentationDAO;
 import org.springframework.web.servlet.mvc.Controller;
@@ -26,6 +27,11 @@ public abstract class SchemaBrowserController implements Controller {
             GusTable table = (GusTable) i.next( );
             table.setDocumentation( getDocumentationDAO( ).getDocumentation( table.getSchema( ).getName( ),
                     table.getName( ) ) );
+	    for ( Iterator j = table.getColumnsExcludeSuperclass( false ).iterator( ); j.hasNext( ); ) {  
+		GusColumn col = (GusColumn) j.next( );  
+		col.setDocumentation( getDocumentationDAO( ).getDocumentation( table.getSchema( ).getName( ),  
+									       table.getName( ), col.getName( ) ) );  
+	    }
         }
     }
 
